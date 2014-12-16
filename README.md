06d6747865617556c83e5514d9c03b5855193237
Chris Freas <cfreas@packetbusters.net>
2014-12-16 10:52:42 -0500

    initial commit. configs included but should be reviewed and put into Ansible

--- README.md
+++ README.md
@@ -2,3 +2,22 @@ fradiusauth
 ===========
 
 Authentication scripts for FreeRADIUS using the Packet API
+
+Setup and testing
+=================
+
+Copy files into appropriate places
+----------------------------------
+
+0. ```cp fradiusauth.py /usr/local/bin```
+1. ```chmod 0755 /usr/local/bin/fradiusauth.py```
+2. ```cp users /etc/freeradius```
+3. ```cp clients.conf /etc/freeradius```
+4. ```service freeradius restart```
+
+Testing
+-------
+
+0. ```git clone https://github.com/chasemp/py-radius.git```
+1. ```cd py-radius; python setup.py install```
+2. ```cd fradiusauth; python libradclient.py <RADIUS host> <RADIUS secret> <username> <password>```
---
+++ fradiusauth.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import requests, sys, syslog, json
+
+script, user, passwd, caller = sys.argv
+passwd = passwd.lstrip("'").rstrip("'")
+
+api_server = "https://api-staging.packet.net:6443/sessions"
+payload = { "login": user, "password": passwd }
+
+msg = 'fradiusauth.py: Authentication attempt by user: ' + user + '\n'
+syslog.syslog(msg)
+
+headers = {'Content-type': 'application/json', "X-Consumer-Token": "365753ceb7b4f472c86da34205e3e12c3a595a9981720b5fac52baf17069ea9e"}
+
+resp = requests.post(api_server, data=json.dumps(payload), headers=headers)
+
+if int(resp.status_code) == 201:
+    print "Accept"
+    sys.exit(0)
+
+print "Reject"
+sys.exit(1)
+
