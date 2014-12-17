7f03a5b6237a3a91c39124f176ae6fa1c5f689a0
Chris Freas <cfreas@packetbusters.net>
2014-12-17 16:32:11 -0500

    [finishes #83244792 #83244816] Add parsing for 2 factor authentication. This is now a requirement for customers to access their projects over a VPN

--- fradiusauth.py
+++ fradiusauth.py
@@ -1,28 +1,42 @@
 #!/usr/bin/env python
 
-import requests, sys, syslog, json
+import requests, sys, syslog, json, re
 
-script, user, passwd, caller = sys.argv
-passwd = passwd.lstrip("'").rstrip("'")
+script, user, token, caller = sys.argv
 
-api_server = "https://api-staging.packet.net:6443/sessions"
-payload = { "login": user, "password": passwd }
+# Split the token into the OTP and password
+token = token.lstrip("'").rstrip("'")
+pattern = re.compile('(\d{6,6})(.*)')
 
-msg = 'fradiusauth.py: Authentication attempt by user: ' + user + '\n'
-syslog.syslog(msg)
+# Now, search for the OTP and password
+match = pattern.search(token)
+if match:
+    otp = match.group(1)
+    passwd = match.group(2)
 
-headers = {'Content-type': 'application/json', "X-Consumer-Token": "365753ceb7b4f472c86da34205e3e12c3a595a9981720b5fac52baf17069ea9e"}
+    api_server = "https://api-staging.packet.net:6443/sessions"
+    payload = { "login": user, "password": passwd }
 
-resp = requests.post(api_server, data=json.dumps(payload), headers=headers)
+    msg = 'Authentication attempt by user: ' + user + '\n'
+    syslog.syslog(msg)
 
-if int(resp.status_code) == 201:
-	msg = 'fradiusauth.py: Authentication for %s successful.' % user
-	syslog.syslog(msg)
-    print "Accept"
-    sys.exit(0)
+    headers = {'Content-type': 'application/json', "X-Consumer-Token": "365753ceb7b4f472c86da34205e3e12c3a595a9981720b5fac52baf17069ea9e", "X-OTP-Token": otp}
 
-msg = 'fradiusauth.py: Authentication failed for %s with API status code %s.' % (user, str(resp.status_code))
+    resp = requests.post(api_server, data=json.dumps(payload), headers=headers)
+
+    if int(resp.status_code) == 201:
+        msg = 'fradiusauth.py: Authentication for %s successful.' % user
+        syslog.syslog(msg)
+        print "Accept"
+        sys.exit(0)
+
+    msg = 'fradiusauth.py: Authentication failed for %s with API status code %s.' % (user, str(resp.status_code))
+    syslog.syslog(msg)
+    print "Reject"
+    sys.exit(1)
+
+# token/password pattern failed, so reject the attempt
+msg = 'fradiusauth.py: Authentication failed for %s due to a bad password or token' % user
 syslog.syslog(msg)
 print "Reject"
 sys.exit(1)
-
